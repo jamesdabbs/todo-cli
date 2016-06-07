1502255579b1f57b65a6329c43631228e81cf38d
James Dabbs <jamesdabbs@gmail.com>
2016-06-07 11:25:02 -0400

    Add list create

--- lib/user.rb
+++ lib/user.rb
@@ -3,24 +3,12 @@ class User < ActiveRecord::Base
   validates_length_of :password, minimum: 4
   validates_uniqueness_of :username
 
-  # def lists
-  #   List.where(user_id: id)
-  # end
   has_many :lists
-
-  # Bad: n+1 query
-  # def items
-  #   all_items = []
-  #   lists.each do |l|
-  #     all_items.push l.items
-  #   end
-  #   all_items.flatten
-  # end
-
-  # Better: 2 queries
-  # def items
-  #   list_ids = lists.pluck(:id)
-  #   Item.where(list_id: list_ids)
-  # end
   has_many :items, through: :lists
+
+  def make_list title
+    # List.create! title: title, user_id: id
+    # List.create! title: title, user: self
+    lists.create! title: title
+  end
 end
