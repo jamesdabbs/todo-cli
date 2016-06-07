1de818d945ad53a36bebb24aec6487838fb6aa33
James Dabbs <jamesdabbs@gmail.com>
2016-06-07 11:18:02 -0400

    Add has_many through

--- lib/user.rb
+++ lib/user.rb
@@ -7,4 +7,20 @@ class User < ActiveRecord::Base
   #   List.where(user_id: id)
   # end
   has_many :lists
+
+  # Bad: n+1 query
+  # def items
+  #   all_items = []
+  #   lists.each do |l|
+  #     all_items.push l.items
+  #   end
+  #   all_items.flatten
+  # end
+
+  # Better: 2 queries
+  # def items
+  #   list_ids = lists.pluck(:id)
+  #   Item.where(list_id: list_ids)
+  # end
+  has_many :items, through: :lists
 end
