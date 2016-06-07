01d24b821b409451b1b34726196a0873670f2f3a
James Dabbs <jamesdabbs@gmail.com>
2016-06-07 10:51:01 -0400

    Add memoized user method

--- lib/list.rb
+++ lib/list.rb
@@ -1,4 +1,13 @@
 class List < ActiveRecord::Base
   validates_presence_of :title, :user_id
   validates_uniqueness_of :title, scope: :user_id
+
+  def user
+    # if @user
+    #   @user
+    # else
+    #   @user = User.find(user_id)
+    # end
+    @user ||= User.find(user_id)
+  end
 end
