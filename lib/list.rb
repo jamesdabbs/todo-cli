629695862a7a67bdbf3f57612ba6a01275da7eef
James Dabbs <jamesdabbs@gmail.com>
2016-06-07 10:54:52 -0400

    Add lists method to users

--- lib/list.rb
+++ lib/list.rb
@@ -2,12 +2,13 @@ class List < ActiveRecord::Base
   validates_presence_of :title, :user_id
   validates_uniqueness_of :title, scope: :user_id
 
-  def user
-    # if @user
-    #   @user
-    # else
-    #   @user = User.find(user_id)
-    # end
-    @user ||= User.find(user_id)
-  end
+  # def user
+  #   # if @user
+  #   #   @user
+  #   # else
+  #   #   @user = User.find(user_id)
+  #   # end
+  #   @user ||= User.find(user_id)
+  # end
+  belongs_to :user
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   validates_presence_of :username, :password
   validates_length_of :password, minimum: 4
   validates_uniqueness_of :username
+
+  def lists
+    List.where(user_id: id)
+  end
 end
