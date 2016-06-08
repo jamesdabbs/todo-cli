65b4dcb2fc2f2ea43ff706101059a5ba926a9bad
James Dabbs <jamesdabbs@gmail.com>
2016-06-08 10:33:38 -0400

    Add first handler

--- run.rb
+++ run.rb
@@ -1,10 +1,19 @@
 require "pry"
+require "./db/setup"
+require "./lib/all"
 
 command = ARGV.shift
+username = `git config user.name`.chomp
+puts "Welcome back, #{username}"
 
 case command
 when "lists"
-  puts "Users wants to see all lists"
+  user  = User.find_by username: username
+  lists = user.lists
+  # FIXME: n+1
+  lists.each do |list|
+    puts "* #{list.title} [#{list.items.count}]"
+  end
 when "show"
   name = ARGV.first
   puts "Users wants to see one list, namely #{name}"
